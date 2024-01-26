cff0091876028de021fb54c91bebab280ca06d43
gonza-lo23 <investingking213@gmail.com>
2024-01-26 13:31:34 +0100

    primet commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,52 @@
+const {response, request} = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+    const {q, nombre = 'no name', apikey, page=1, limit} = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg: 'put API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+const usuariosDelete = (req, res = response) => {
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg: 'put Delete - controlador',
+        nombre,
+        edad
+    });
+}
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete
+}
